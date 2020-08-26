a53e718d395257a1442fd2f3aa10da505ea87720
felixoppongwillborg <fwlixw@gmail.com>
2020-08-26 12:18:37 +0200

    created specfile and wrote first test to return a 200 response and a list of books. all test passing

---
+++ spec/requests/booksEndpoint.spec.js
@@ -0,0 +1,31 @@
+const app = require("../../app");
+const supertest = require("supertest");
+const expect = require("chai").expect;
+
+let server, request, response;
+
+before((done) => {
+  server = app.listen(done);
+  request = supertest.agent(server);
+});
+
+after((done) => {
+  server.close(done);
+});
+
+describe("GET /api/v1/books", () => {
+  before(async () => {
+    response = await request.get("/api/v1/books");
+  });
+
+  it("is expected to respond with status 200", () => {
+    expect(response.status).to.equal(200);
+  });
+
+  it("is expected to respond with list of books", async () => {
+    const expectedResponse = {
+      books: [{ title: "Felix life" }],
+    };
+    expect(response.body).to.deep.equal(expectedResponse);
+  });
+});
