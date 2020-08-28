a01bed212ff267923d80f9c516745747db749084
felixoppongwillborg <fwlixw@gmail.com>
2020-08-28 16:03:39 +0200

    changed seedfile in db

--- database/seeders/20200826124509-Book.js
+++ database/seeders/20200826124509-Book.js
@@ -1,26 +1,39 @@
 "use strict";
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("Books", null, {});
+    await queryInterface.bulkDelete("Authors", null, {});
+    await queryInterface.bulkInsert("Authors", [
+      {
+        firstName: "Felix",
+        lastName: "Oppong Willborg",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+    const authors = await queryInterface.sequelize.query(
+      `SELECT id from "Authors";`
+    )
+    const authorsRows = authors[0]
     await queryInterface.bulkInsert(
       "Books",
       [
         {
-          title: "Learn NodeJS with CraftAcademy - in test",
+          title: "Learn NodeJS with CraftAcademy",
           createdAt: new Date(),
           updatedAt: new Date(),
+          AuthorId: authorsRows[0].id,
         },
         {
-          title: "Learn Sequelize with Adi - in test",
+          title: "Learn Sequelize with Adi",
           createdAt: new Date(),
           updatedAt: new Date(),
+          AuthorId: authorsRows[0].id,
         },
       ],
       {}
     );
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("Books", null, {});
   },
